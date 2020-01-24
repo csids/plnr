b4893c1eb489b8a1ce075321deb1805ed315eed7
Travis CI User <travis@example.org>
2020-01-24 20:42:32 +0000

    Travis CI update 22 [ci skip]

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: plnr")
-  packageStartupMessage("Version 2020.01.22 at 10:00")
+  packageStartupMessage("Version 2020.01.24 at 20:40")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
