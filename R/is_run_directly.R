b3179a0bb1f5c0cd25b6f81d8a92a505a71a2310
Richard White <w@rwhite.no>
2020-05-04 22:33:12 +0200

    small fix

--- R/is_run_directly.R
+++ R/is_run_directly.R
@@ -4,5 +4,12 @@
 #' or if it is being run directly
 #' @export
 is_run_directly <- function() {
-  return(match.call(call = sys.call(sys.parent(1)))[[1]] == "is_run_directly")
+  return(
+    match.call(call = sys.call(sys.parent(1)))[[1]] %in%
+      c(
+        "is_run_directly",
+        "plnr::is_run_directly",
+        "plnr:::is_run_directly"
+      )
+  )
 }
