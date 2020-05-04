a819ea20ff7fd065e0683e7989e55e74268845f0
Richard White <w@rwhite.no>
2020-05-04 21:58:23 +0200

    is_run_directly function created, allowing the user to see if their code is being run directly or from within a function

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# plnr 2020.5.4
+
+- is_run_directly function created, allowing the user to see if their code is being run directly or from within a function
+
 # plnr 2020.4.3
 
 - set_opts function created, allowing for force_verbose to be set package wide
---
+++ R/is_run_directly.R
@@ -0,0 +1,8 @@
+#' Is this code run directly?
+#'
+#' This function determines if it is being called from within a function
+#' or if it is being run directly
+#' @export
+is_run_directly <- function(){
+  return(match.call(call = sys.call(sys.parent(1)))[[1]]=="is_run_directly")
+}
