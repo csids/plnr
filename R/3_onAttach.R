4dc6cc9811d1c8ad663545ace71ca59e53c95c22
Travis CI User <travis@example.org>
2020-05-11 16:24:40 +0000

    Travis CI update 33 [ci skip]

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: plnr")
-  packageStartupMessage("Version 2020.05.04 at 21:14")
+  packageStartupMessage("Version 2020.05.11 at 16:19")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
--- R/get_anything.R
+++ R/get_anything.R
@@ -1,9 +1,9 @@
 #' Gets anything
 #' @param x the string that we are getting
 #' @export
-get_anything <- function(x){
-  if(length(grep("::", x))>0) {
-    parts<-strsplit(x, "::")[[1]]
+get_anything <- function(x) {
+  if (length(grep("::", x)) > 0) {
+    parts <- strsplit(x, "::")[[1]]
     getExportedValue(parts[1], parts[2])
   } else {
     get(x)
