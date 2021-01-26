fcaa5cf02a8673bdc1da41bfbb58225cf4a951eb
Richard White <hello@rwhite.no>
2021-01-26 06:25:15 +0100

    on attach

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,5 +1,11 @@
+#' @import data.table ggplot2
+#' @importFrom magrittr %>%
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("PACKAGE: plnr")
-  packageStartupMessage("Version 2020.09.24 at 12:37")
-  packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
+  .onAttach <- function(libname, pkgname) {
+    packageStartupMessage(paste(
+      "plnr",
+      utils::packageDescription("plnr")$Version,
+      "https://folkehelseinstituttet.github.io/plnr"
+    ))
+  }
 }
