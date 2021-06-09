98c23dc32b2b1a62b7fad1b27c0ed947e1fcccf4
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-06-09 05:26:22 +0200

    easy_split

--- R/split_equal.r
+++ R/split_equal.r
@@ -1,7 +1,20 @@
-#' split_equal
+#' easy_split
 #' @param x a
 #' @param size a
+#' @examples
+#' easy_split(letters[1:20], size_of_each_group = 3)
+#' easy_split(letters[1:20], number_of_groups = 3)
 #' @export
-split_equal <- function(x, size = 10) {
-  split(x, ceiling(seq_along(x) / size))
+easy_split <- function(x, size_of_each_group = NULL, number_of_groups = NULL) {
+  if(is.null(size_of_each_group) & is.null(number_of_groups)) stop("you must specify ONE of size_of_each_group OR number_of_groups")
+  if(!is.null(size_of_each_group) & !is.null(number_of_groups)) stop("you must specify ONE of size_of_each_group OR number_of_groups")
+
+  if(!is.null(size_of_each_group)){
+    return(split(x, ceiling(seq_along(x) / size_of_each_group)))
+  }
+
+  if(!is.null(number_of_groups)){
+    splitting_index <- rep(1:number_of_groups, each = ceiling(length(x)/number_of_groups))[1:length(x)]
+    return(split(x, splitting_index))
+  }
 }
