933a7fc1b707799c8cf800c6b880f0574695a1c2
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-02-09 12:07:07 +0100

    onAttach

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,7 +1,14 @@
 .onAttach <- function(libname, pkgname) {
+  version <- tryCatch(
+    utils::packageDescription("plnr", fields = "Version"),
+    warning = function(w){
+      1
+    }
+  )
+
   packageStartupMessage(paste0(
     "plnr ",
-    utils::packageDescription("plnr")$Version,
+    version,
     "\n",
     "https://docs.sykdomspulsen.no/plnr"
   ))
