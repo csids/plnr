b32aff1748087926de9af0e5e7b46ceb569e325f
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-06-03 12:19:18 +0200

    Documentation

--- R/expand_list.R
+++ R/expand_list.R
@@ -1,6 +1,12 @@
 #' Expand (cross) lists
+#'
 #' The same as purrr::cross, but doesn't require an extra list()
 #' @param ... Dots
+#' @examples
+#' plnr::expand_list(
+#'   a = 1:2,
+#'   b = c("a", "b")
+#' )
 #' @export
 expand_list <- function(...) {
   dots <- list(...)
--- R/get_anything.R
+++ R/get_anything.R
@@ -1,5 +1,10 @@
-#' Gets anything
+#' Gets anything (including with package scoping)
+#'
+#' base::get does not work with package scoping (e.g. get("pkg::var")).
+#' plnr::get_anything works with package scoping.
 #' @param x the string that we are getting
+#' @examples
+#' plnr::get_anything("plnr::norway_covid19_cases_by_time_location")
 #' @export
 get_anything <- function(x) {
   if (length(grep("::", x)) > 0) {
